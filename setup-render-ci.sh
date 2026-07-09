ee5ce2e6de295a73be7a3bb27c0fff3e7a3fd90f
Julien Chiquet <julien.chiquet@inrae.fr>
2026-07-09 09:38:14 +0200

    try librsvg2-bin to replace inkscape

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,5 +1,1 @@
-sudo apt-get install -y inkscape
-mkdir -p ~/.local/bin
-~/.TinyTeX/bin/x86_64-linux/tlmgr option sys_bin ~/.local/bin
-~/.TinyTeX/bin/x86_64-linux/tlmgr path add
-~/.TinyTeX/bin/x86_64-linux/tlmgr update --self
+sudo apt-get install -y librsvg2-bin
